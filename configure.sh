48bf77f3485b6b6c35a7ad8220125054cbe126af
Sarika891 <sourabh.sharma@ip-10-110-50-170.eu-central-1.compute.internal>
2018-12-28 15:23:24 +0000

    Add new file configure

---
+++ configure.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.42.tar.gz
+tar -zxf pcre-8.42.tar.gz
+cd pcre-8.42
+./configure
+make
+sudo make install
+wget http://zlib.net/zlib-1.2.11.tar.gz
+tar -zxf zlib-1.2.11.tar.gz
+cd zlib-1.2.11
+./configure
+make
+sudo make install
+wget http://www.openssl.org/source/openssl-1.0.2q.tar.gz
+tar -zxf openssl-1.0.2q.tar.gz
+cd openssl-1.0.2q
+./Configure darwin64-x86_64-cc --prefix=/usr
+make
+sudo make install
